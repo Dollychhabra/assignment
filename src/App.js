b1b7d89af6703f626f6668c0c29caec8564624e6
Dollychhabra <dolly.chhabra@prontoitlabs.com>
2019-02-02 23:18:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      arr: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
+      newArr: [],
+      start: 8
+    }
+  }
+
+  componentDidMount() {
+    this.main();
+  }
+
+
+  main = () => {
+    let { arr, newArr } = this.state;
+    for (var i = 0; i < 8; i++) {
+      newArr.push(arr[i]);
+      this.setState({ newArr });
+    }
+  }
+
+
+  clickMe = () => {
+
+    let { arr, newArr, start } = this.state;
+    newArr = [];
+    for (var i = 0; i < 8; i++) {
+
+      if (start >= arr.length) {
+        start = 0;
+        this.setState({ start })
+
+      }
+      newArr.push(arr[start]);
+      this.setState({ newArr });
+
+      start++;
+      this.setState({ start })
+    }
+
+  }
+
   render() {
+    let { newArr } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="grids">
+        <ul className="grids__ul">
+          {newArr.map(function (data, idx) {
+            return (
+              <li className="grids__li" key={idx}>{data}</li>
+            )
+          })}
+          <button className="grids__button" onClick={() => this.clickMe()}>Click me</button>
+        </ul>
       </div>
     );
   }
